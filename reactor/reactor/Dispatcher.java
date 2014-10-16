4943ccae6e44b3c226eb06c6f5487b0b48acb7a1
Nicholas Allio <nicholas.allio@me.com>
2014-10-16 17:58:33 +0300

    Dispatcher implemented

--- reactor/reactor/Dispatcher.java
+++ reactor/reactor/Dispatcher.java
@@ -1,33 +1,47 @@
 package reactor;
 
+import java.util.List;
+
 import reactorapi.*;
 
 public class Dispatcher {
+
+	private List<EventHandler<?>> eventHandlerList;
+	private BlockingEventQueue<Object> blockingQueue;
+	private List<WorkerThread<?>> workerThreadList;
+
 	public Dispatcher() {
-		this(10);
+		this.blockingQueue = new BlockingEventQueue<>(10);
 	}
 
 	public Dispatcher(int capacity) {
-		// TODO: Implement Dispatcher(int).
+		this.blockingQueue = new BlockingEventQueue<>(capacity);
 	}
 
 	public void handleEvents() throws InterruptedException {
-		// TODO: Implement Dispatcher.handleEvents().
+		while (true) {
+			Event<?> e = select();
+			if (eventHandlerList.contains(e.getHandler())) {
+				e.handle();
+			}
+		}
 	}
 
 	public Event<?> select() throws InterruptedException {
-		throw new UnsupportedOperationException();
-		// TODO: Implement Dispatcher.select().
+		Event<?> e = this.blockingQueue.get();
+		return e;
 	}
 
 	public void addHandler(EventHandler<?> h) {
-		// TODO: Implement Dispatcher.addHandler(EventHandler).
-		//hello
+		WorkerThread<?> wThread = new WorkerThread<>(h, this.blockingQueue);
+		eventHandlerList.add(h);
+		wThread.start();
+		workerThreadList.add(eventHandlerList.indexOf(h), wThread);
 	}
 
 	public void removeHandler(EventHandler<?> h) {
-		// TODO: Implement Dispatcher.removeHandler(EventHandler).
+		int workerIndex = workerThreadList.lastIndexOf(h);
+		workerThreadList.get(workerIndex).cancelThread();
+		eventHandlerList.remove(h);
 	}
-
-	// Add methods and fields as needed.
 }
